ee50addaee7b7aa5c7b0305210875f489154791f
blanck <samuel.blanck@univ-lille2.fr>
2017-05-11 16:45:44 +0200

    remove rdata and cel datatypes

--- smagexp_datatypes/datatypes_smagexp.py
+++
@@ -1,7 +0,0 @@
-from galaxy.datatypes import data
-from galaxy.datatypes.binary import Binary
-
-class Cel( Binary ):
-    """Class for generic CEL binary format"""
-    file_ext = "cel"
-Binary.register_unsniffable_binary_ext("cel")
